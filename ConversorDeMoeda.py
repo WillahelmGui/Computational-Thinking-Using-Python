aea845818832dd0aeac28e4a04ca392f8fcc367a
WillahelmGui <ribeiro.guicosta@gmail.com>
2025-03-19 20:40:09 -0300

    Cria pseudocódigo e código em Python

---
+++ ConversorDeMoeda.py
@@ -0,0 +1,7 @@
+quantidadeDeReais = 0
+cotacaoDolar = 0
+resultado = 0
+cotacaoDolar = float(input("Informe a cotacao do dolar: \n"))
+quantidadeDeReais = float(input("Informe a quantidade de reais: \n"))
+resultado = quantidadeDeReais / cotacaoDolar
+print("Quantidade de Dolares comprados: ", resultado)
